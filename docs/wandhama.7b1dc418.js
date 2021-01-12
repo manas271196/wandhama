c4bdbfe1a9828b33c0294b08991398538f6d234d
Manas Sharma <manas.sharma@thomsonreuters.com>
2021-01-12 12:24:57 +0530

    fixes

--- docs/wandhama.7b1dc418.js
+++ docs/wandhama.7b1dc418.js
@@ -31007,7 +31007,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51573" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62272" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
